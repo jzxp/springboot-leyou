6df4236dc73e6e537f6a349c8fe9e7d9bccef199
Xie Jun <1124685815@qq.com>
2021-02-04 20:52:29 +0800

    新增User类和Api

--- springboot-leyou-user/springboot-leyou-user-interface/src/main/java/com/juzipi/demo/pojo/User.java
+++ springboot-leyou-user/springboot-leyou-user-interface/src/main/java/com/juzipi/demo/pojo/User.java
@@ -3,11 +3,13 @@ package com.juzipi.demo.pojo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Pattern;
 import java.util.Date;
 
 
@@ -18,11 +20,15 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Length(min = 4,max = 30,message = "用户名长度在4-30为之间")
     private String username;// 用户名
 
+    @Length(min = 4,max = 30,message = "密码长度在4-30为之间")
     @JsonIgnore//对象序列化为json字符串时，忽略该属性
     private String password;// 密码
 
+    //专业: ^((13[0-9])|(14[5-9])|(15([0-3]|[5-9]))|(16[6-7])|(17[1-8])|(18[0-9])|(19[1|3])|(19[5|6])|(19[8|9]))\d{8}$
+    @Pattern(regexp = "^1[356789]\\d{9}$",message = "手机号不合法")
     private String phone;// 电话
 
     private Date created;// 创建时间
