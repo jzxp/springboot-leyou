817bb405e41938a95ddc8d42b350afd4165c2c85
Xie Jun <1124685815@qq.com>
2021-02-04 20:47:41 +0800

    新增根据用户名和密码查询用户

--- springboot-leyou-user/springboot-leyou-user-service/src/main/java/com/juzipi/demo/controller/UserController.java
+++ springboot-leyou-user/springboot-leyou-user-service/src/main/java/com/juzipi/demo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.juzipi.demo.controller;
 
+import com.juzipi.demo.pojo.User;
 import com.juzipi.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -7,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 public class UserController {
@@ -42,5 +45,37 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+
+    /**
+     * 注册
+     * @param user
+     * @param code
+     * @return
+     */
+    @PostMapping("register")
+    public ResponseEntity<Void> register(@Valid User user, @RequestParam("code")String code){
+
+        this.userService.register(user, code);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+
+    /**
+     * 根据用户名和密码查询用户
+     * @param username
+     * @param password
+     * @return
+     */
+    @GetMapping("query")
+    public ResponseEntity<User> queryUser(
+            @RequestParam("username")String username,
+            @RequestParam("password")String password
+    ){
+        User user = this.userService.queryUser(username,password);
+        if (StringUtils.isEmpty(user)){
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(user);
+    }
 
 }
--- springboot-leyou-user/springboot-leyou-user-service/src/main/java/com/juzipi/demo/service/UserService.java
+++ springboot-leyou-user/springboot-leyou-user-service/src/main/java/com/juzipi/demo/service/UserService.java
@@ -2,14 +2,15 @@ package com.juzipi.demo.service;
 
 import com.juzipi.demo.mapper.UserMapper;
 import com.juzipi.demo.pojo.User;
+import com.juzipi.demo.utils.CodecUtils;
 import com.juzipi.demo.utils.NumberUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -67,6 +68,63 @@ public class UserService {
         //把验证码保存到redis中
         this.stringRedisTemplate.opsForValue().set(KEY_PREFIX+phone,code,3, TimeUnit.MINUTES);
 
+
+    }
+
+
+
+    /**
+     * 注册
+     * @param user
+     * @param code
+     * @return
+     */
+    public void register(User user, String code) {
+        //查询redis中的验证码
+        String redisCode = this.stringRedisTemplate.opsForValue().get(KEY_PREFIX + user.getPhone());
+
+        //校验验证码
+        if (!StringUtils.equals(code,redisCode)){
+            return;
+        }
+        //生成盐
+        String salt = CodecUtils.generateSalt();
+        user.setSalt(salt);
+        //加盐加密
+        user.setPassword(CodecUtils.md5Hex(user.getPassword(),salt));
+        //新增用户
+        user.setId(null);
+        user.setCreated(new Date());//创建时间
+        this.userMapper.insertSelective(user);
+
+
+    }
+
+
+
+    /**
+     * 根据用户名和密码查询用户
+     * @param username
+     * @param password
+     * @return
+     */
+    public User queryUser(String username, String password) {
+        User record = new User();
+        record.setUsername(username);
+
+
+        User user = this.userMapper.selectOne(record);
+        //判断user是否为空
+        if (org.springframework.util.StringUtils.isEmpty(user)) {
+            return null;
+        }
+        //获取盐，对用户输入密码加盐加密
+        password = CodecUtils.md5Hex(password,user.getSalt());
+        //和数据库中的密码比较
+        if (StringUtils.equals(password,user.getPassword())){
+            return user;
+        }
+        return null;
 
     }
 }
